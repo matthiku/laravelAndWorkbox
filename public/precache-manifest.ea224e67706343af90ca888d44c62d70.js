0023e216f1c825c52d75f42ff90a217b53b4fa8d
Matthias Kuhs <matthiku@yahoo.com>
2018-06-11 11:36:11 +0100

    initial commit

---
+++ public/precache-manifest.ea224e67706343af90ca888d44c62d70.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "6097d3e623c31df98213",
+    "url": "./js/app.js"
+  },
+  {
+    "revision": "6097d3e623c31df98213",
+    "url": "./css/app.css"
+  }
+];
